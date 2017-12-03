aa32ce9068d78b9b4a4b2bbf08caa888eebd6052
petrarch1603 <petrarch1603@gmail.com>
2017-12-03 12:25:27 -0700

    remove superfluous documentation

--- Congratulations.py
+++ Congratulations.py
@@ -1,31 +1,5 @@
 from functions import *
 
-#                                              * Congratulations Post *
-#
-#                         * Purpose: Post the winners from the /r/MapPorn voting contest. *
-#
-#   Every month we have a monthly map contest. Users can submit a map at any time. The users send a submission
-#   to a Reddit bot's inbox. A raspberry pi runs a script every hour checking that inbox. When the bot receives a
-#   submission, it parses the message and saves a csv with the following fields:
-#
-#                       * Map Name
-#                       * Map Image URL
-#                       * Map Description
-#                       * Map Author
-#                       * Unique identifier for the submission
-#
-#   This CSV is used to create a Reddit contest post at the end of the month. The script 'votingpost.py' parses this
-#   CSV and creates a comment for each map submission. Users can upvote or downvote each comment. After about a week,
-#   the contest ends. Then the script 'Congratulations.py' is run. This script gets the four highest rated comments
-#   (maps) and uses that data to make a new post to congratulate the winners. The old CSV is archived for a yearly
-#   contest at the end of the year.
-#
-#   Rules and information about the contest can be found here: https://www.reddit.com/r/MapPorn/wiki/meta/contest
-#
-#
-#                                                                   Created by /u/Petrarch1603
-#                                                                   2017 November
-
 # Verify before running in case of accidental execution.
 # If this script is automated, this line will need to be deleted.
 input('Are you ready?')
