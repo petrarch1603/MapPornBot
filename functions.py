1dd1a0bc8e6fbd2a9dd0a2ac8d848b85dde11db3
petrarch1603 <petrarch1603@gmail.com>
2017-12-16 09:07:56 -0700

    pep compliance

--- functions.py
+++ functions.py
@@ -84,19 +84,19 @@ def parse_reddit(raw_id, twitter_char_limit):
 
 
 def remove_text_inside_brackets(text, brackets="[]"): #()
-    count = [0] * (len(brackets) // 2) # count open/close brackets
+    count = [0] * (len(brackets) // 2)  # count open/close brackets
     saved_chars = []
     for character in text:
         for i, b in enumerate(brackets):
-            if character == b: # found bracket
+            if character == b:  # found bracket
                 kind, is_close = divmod(i, 2)
-                count[kind] += (-1)**is_close # `+1`: open, `-1`: close
-                if count[kind] < 0: # unbalanced bracket
+                count[kind] += (-1)**is_close  # `+1`: open, `-1`: close
+                if count[kind] < 0:  # unbalanced bracket
                     count[kind] = 0  # keep it
                 else:  # found bracket to remove
                     break
         else: # character is not a [balanced] bracket
-            if not any(count): # outside brackets
+            if not any(count):  # outside brackets
                 saved_chars.append(character)
     return ''.join(saved_chars)
 
