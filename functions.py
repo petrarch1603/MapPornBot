2317e722bfdc043cafcae926521fabe0516e395f
petrarch1603 <petrarch1603@gmail.com>
2017-12-29 14:08:47 -0700

    add announcement to shotgunblast

--- functions.py
+++ functions.py
@@ -63,7 +63,7 @@ def shotgun_blast(raw_id_input, announce_input):
     #         print(messageshort)
     #     else:
     #         pass
-    messageshort = announce + messageshort + '\n' + shortlink
+    messageshort = announce + '\n' + messageshort + '\n' + shortlink
     print('Message Long = ' + str(messagelong) + ' (' + str(len(messagelong)) + ')')
     xy = post_from_reddit(url, messageshort, raw_id, messagelong)
     return xy
