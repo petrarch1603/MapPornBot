ad7b670166e26d333c872422bed80874df91eaf0
petrarch1603 <petrarch1603@gmail.com>
2018-01-07 16:57:10 -0700

    add logging to top year ago

--- yearagotop.py
+++ yearagotop.py
@@ -1,27 +1,32 @@
 from time import mktime
 from functions import *
-import datetime
+import time
 
 yearago = datetime.datetime.now() - datetime.timedelta(days=1*365)
 lessthanyearago = datetime.datetime.now() - datetime.timedelta(days=1*360)
 highesttimestamp = int(mktime(yearago.timetuple()))
 lowesttimestamp = int(mktime(lessthanyearago.timetuple()))
 
-loglist = []
-loglist.append(datetime.datetime.now())
-
 subreddit = r.subreddit('mapporn')
 for submission in subreddit.submissions(highesttimestamp, lowesttimestamp):
     raw_id = submission
     break
 
-try:
-    post = shotgun_blast(raw_id_input=raw_id, announce_input='')
-    loglist.append(post)
-except:
-    loglist.append('Unable to post yearagotop to social media')
+logdict = {'type': 'socmediapost'}
 
-with open('logs/prettylog.txt', 'a') as logfile:
-    for i in loglist:
-        logfile.write(str(i))
-    logfile.write('-------------')
+
+try:
+    social_media_post = shotgun_blast(raw_id_input=raw_id, announce_input='')
+    logdict['time'] = time.time()
+    logobject = {'script': 'Top Year Ago',
+                 'message': str(social_media_post.message),
+                 'tweet_url': str(social_media_post.tweet_url),
+                 'tumblr_url': str(social_media_post.tumblr_url),
+                 'fb_url': str(social_media_post.facebook_url)}
+    logdict['object'] = logobject
+    addToJSON(logdict)
+
+except Exception as ex:
+    logdict['time'] = time.time()
+    logdict['error'] = str(ex)
+    addToJSON(logdict)
