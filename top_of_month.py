edb6817b82fa2669b9a589a5ebdb425f238529c2
petrarch1603 <petrarch1603@gmail.com>
2018-01-07 16:54:44 -0700

    add logging to top of month

--- top_of_month.py
+++ top_of_month.py
@@ -1,22 +1,26 @@
 from functions import *
+import time
 
 # Post the top /r/MapPorn submission from the last month.
 
-loglist = []
-loglist.append(datetime.datetime.now())
 
 top = r.subreddit('mapporn').top('month', limit=1)
 top = list(top)
 top_month = (top[0])
 announce_month = 'Top post of the month:\n'
+logdict = {'type': 'socmediapost'}
 
 try:
-    output = shotgun_blast(raw_id_input=top_month, announce_input=announce_month)
-    loglist.append(output)
-except:
-    loglist.append('Unable to post top post of the month')
-
-with open('logs/prettylog.txt', 'a') as logfile:
-    for i in loglist:
-        logfile.write(str(i))
-    logfile.write('-------------')
+    social_media_post = shotgun_blast(raw_id_input=top_month, announce_input=announce_month)
+    logdict['time'] = time.time()
+    logobject = {'script': 'Top Post of Month',
+                 'message': str(social_media_post.message),
+                 'tweet_url': str(social_media_post.tweet_url),
+                 'tumblr_url': str(social_media_post.tumblr_url),
+                 'fb_url': str(social_media_post.facebook_url)}
+    logdict['object'] = logobject
+    addToJSON(logdict)
+except Exception as ex:
+    logdict['time'] = time.time()
+    logdict['error'] = str(ex)
+    addToJSON(logdict)
