0be12ddbe8b909e0b6a24cafd0d7052b98433846
petrarch1603 <petrarch1603@gmail.com>
2018-02-05 18:25:42 -0700

    make visually appealing

--- oldmaps_topoftheday.py
+++ oldmaps_topoftheday.py
@@ -6,8 +6,10 @@ logdict = {'type': 'socmediapost'}
 try:
     (subreddit, subtitle, social_media_post) = subreddit_top_post(subreddit='oldmaps', time_window='week')
     logdict['time'] = time.time()
-    logobject = {'script': 'Old Maps Top Post of Week', 'message': str(social_media_post.message),
-                 'tweet_url': str(social_media_post.tweet_url), 'tumblr_url': str(social_media_post.tumblr_url),
+    logobject = {'script': 'Old Maps Top Post of Week',
+                 'message': str(social_media_post.message),
+                 'tweet_url': str(social_media_post.tweet_url),
+                 'tumblr_url': str(social_media_post.tumblr_url),
                  'fb_url': str(social_media_post.facebook_url)}
     logdict['object'] = logobject
     addToMongo(logdict)
--- papertowns_topoftheday.py
+++ papertowns_topoftheday.py
@@ -6,8 +6,10 @@ logdict = {'type': 'socmediapost'}
 try:
     (subreddit, subtitle, social_media_post) = subreddit_top_post(subreddit='papertowns', time_window='day')
     logdict['time'] = time.time()
-    logobject = {'script': 'Papertowns Top Post of Week', 'message': str(social_media_post.message),
-                 'tweet_url': str(social_media_post.tweet_url), 'tumblr_url': str(social_media_post.tumblr_url),
+    logobject = {'script': 'Papertowns Top Post of Week',
+                 'message': str(social_media_post.message),
+                 'tweet_url': str(social_media_post.tweet_url),
+                 'tumblr_url': str(social_media_post.tumblr_url),
                  'fb_url': str(social_media_post.facebook_url)}
     logdict['object'] = logobject
     addToMongo(logdict)
