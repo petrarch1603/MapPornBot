e5592077efae4449a5875efc5e97d1e030d9f961
petrarch1603 <petrarch1603@gmail.com>
2018-08-25 14:32:22 -0600

    initial commit: umbrella script to get status update on different scripts currently programmed to check the historyDB integrity

---
+++ status.py
@@ -0,0 +1,9 @@
+from functions import SQLiteFunctions
+import sqlite3
+
+conn = sqlite3.connect('dayinhistory.db')
+curs = conn.cursor()
+
+historyDBstatus = SQLiteFunctions.check_historyDB_integrity()
+historyDB_rows = SQLiteFunctions.total_rows(cursor=curs, table_name='historymaps')
+print(historyDB_rows)
