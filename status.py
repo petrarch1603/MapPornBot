31ec80d7b9825021a1bca0a4351296d1ca7613b7
petrarch1603 <petrarch1603@gmail.com>
2018-08-26 21:00:49 -0600

    check socmediaDB status

--- status.py
+++ status.py
@@ -6,7 +6,11 @@ curs = conn.cursor()
 
 historyDBstatus = SQLiteFunctions.check_historyDB_integrity()
 historyDB_rows = SQLiteFunctions.total_rows(cursor=curs, table_name='historymaps')
+socmediaDBstatus = SQLiteFunctions.check_socmediaDB_integrity()
+
 print(historyDB_rows)
 print(historyDBstatus)
+print(socmediaDBstatus)
+
 refreshed = SQLiteFunctions.make_fresh_again()
 print(refreshed)
