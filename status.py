caa986a27d241daf22051fd42171ae18f030185d
petrarch1603 <petrarch1603@gmail.com>
2018-08-26 21:05:32 -0600

    check history and socmedia db and pretty print status

--- status.py
+++ status.py
@@ -1,16 +1,20 @@
 from functions import SQLiteFunctions
 import sqlite3
 
-conn = sqlite3.connect('data/dayinhistory.db')
-curs = conn.cursor()
+history_conn = sqlite3.connect('data/dayinhistory.db')
+history_curs = history_conn.cursor()
+
+soc_conn = sqlite3.connect('data/socmedia.db')
+soc_curs = soc_conn.cursor()
 
 historyDBstatus = SQLiteFunctions.check_historyDB_integrity()
-historyDB_rows = SQLiteFunctions.total_rows(cursor=curs, table_name='historymaps')
+historyDB_rows = SQLiteFunctions.total_rows(cursor=history_curs, table_name='historymaps')
 socmediaDBstatus = SQLiteFunctions.check_socmediaDB_integrity()
-
-print(historyDB_rows)
+socmediaDB_rows = SQLiteFunctions.total_rows(cursor=soc_curs, table_name='socmediamaps')
 print(historyDBstatus)
+print("History Rows: " + str(historyDB_rows))
 print(socmediaDBstatus)
+print("Social Media Rows: " + str(socmediaDB_rows))
 
 refreshed = SQLiteFunctions.make_fresh_again()
 print(refreshed)
