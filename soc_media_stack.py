b884f8d73857e6fee3f253b3615370d04e2b1612
petrarch1603 <petrarch1603@gmail.com>
2018-08-27 17:06:23 -0600

    testing db cursor

--- soc_media_stack.py
+++ soc_media_stack.py
@@ -48,6 +48,7 @@ def get_map(target):  # Get a map that is in the target hour range
         random_int = random.randint(0, (len(targetmaplist) - 1))
         my_map = targetmaplist[random_int]
     my_map = my_map.fetchone()
+    #TODO: maybe I should get the reddit object here and verify I have it.
     try:
         print("Title: " + str(my_map[1]))
     except Exception:
