6e59eb7010897292a0abafb040cc8c1dbcbf7d5e
petrarch1603 <petrarch1603@gmail.com>
2018-08-26 20:55:48 -0600

    minor pep compliance

--- soc_media_stack.py
+++ soc_media_stack.py
@@ -47,7 +47,7 @@ def get_map(target):  # Get a map that is in the target hour range
     else:
         random_int = random.randint(0, (len(targetmaplist) - 1))
         my_map = targetmaplist[random_int]
-    #my_map = my_map.fetchone()
+    # my_map = my_map.fetchone()
     return my_map
 
 
