ad111a2df070d34a7a2135f49444fdb13004c7a3
petrarch1603 <petrarch1603@gmail.com>
2018-08-30 21:38:27 -0600

    history.py now uses the database

--- history.py
+++ history.py
@@ -1,29 +1,33 @@
 import calendar
+from classes import *
 import datetime
 from functions import shotgun_blast, send_reddit_message_to_self
+import os
 import praw
 import random
-import sqlite3
 
-# Connect to database
-conn = sqlite3.connect('data/dayinhistory.db')
-curs = conn.cursor()
 
-# Connect to PRAW
-r = praw.Reddit('bot1')
+print("Running This Day in History script")
 
-# Get today's date as day of year (1-366)
-today_int = datetime.datetime.now().timetuple().tm_yday
-if today_int > 60 and calendar.isleap(datetime.datetime.now().year):
-    today_int -= 1
 
-# Get all entries with today's date
-today_list = []
-for row in curs.execute("SELECT * FROM historymaps"):
-    if (row[2]) == today_int:
-        today_list.append(row)
+def init():
+    global hist_db, log_db, my_diag, r, today_int
+    hist_db = HistoryDB()
+    log_db = LoggingDB()
+    my_diag = Diagnostic(script=os.path.basename(__file__))
+    my_diag.table = 'historymaps'
+    r = praw.Reddit('bot1')
+    today_int = datetime.datetime.now().timetuple().tm_yday
+    if today_int > 60 and calendar.isleap(datetime.datetime.now().year):
+        today_int -= 1
+
+
+init()
+today_list = hist_db.get_rows_by_date(today_int)
 
 if len(today_list) == 0:
+    log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=1)
+    log_db.close()
     exit()
 
 # Check if there are multiple entries with today's date
@@ -32,18 +36,17 @@ if len(today_list) > 1:
     random_int = random.randint(0, (len(today_list) - 1))
 
 raw_id = today_list[random_int][0][-6:]
+my_diag.raw_id = raw_id
 redditobject = r.submission(id=raw_id)
 my_title = today_list[random_int][1]
 
-print(redditobject)
-print(my_title)
-
 try:
     x = shotgun_blast(raw_id_input=redditobject, title=my_title)
-    my_message = ("Posted to Twitter: " + str(x.tweet_url))
+    my_diag.tweet = x.tweet_url
+    log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=1)
 except Exception as shotgun_error:
     my_message = ('Error encountered: \n' + str(shotgun_error))
-
-send_reddit_message_to_self(title="This day in history script", message=my_message)
-
-# TODO Add logging to a database
+    my_diag.severity = 2
+    my_diag.traceback = shotgun_error
+    log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=0, error_text=my_message)
+    send_reddit_message_to_self(title="Problem posting this day in history", message=my_message)
