6cce3f22251ad694714375a2b5af0a1b6c6f7f58
petrarch1603 <petrarch1603@gmail.com>
2018-09-07 14:24:49 -0600

    minor pep formatting

--- WeeklySubmit.py
+++ WeeklySubmit.py
@@ -8,11 +8,13 @@ from classes import *
 import os
 import random
 import fnmatch
-from functions import *
-import time
+# from functions import *
+# import time
 
 log_db = LoggingDB()
 my_diag = Diagnostic(script=str(os.path.basename(__file__)))
+title = "Now taking entries for the /r/MapPorn Monthly Map Contest\nSubmit a map here:\n" \
+            "https://www.reddit.com/r/MapPorn/wiki/meta/contest#wiki_submit_a_map"
 
 try:
     # Create a random number for choosing image
@@ -25,10 +27,6 @@ try:
     # Sometimes the image will be a png or jpg or jpeg etc, this will find the extension and return the whole filename.
     image_file_name = fnmatch.filter(os.listdir('.'), image_file_name + '.*')  # This looks in the directory and creates a list of files with the name of the image.
     image_file = image_file_name[0]  # There should only be one image with that name, so this returns the name of that file.
-
-    title = "Now taking entries for the /r/MapPorn Monthly Map Contest\nSubmit a map here:\n" \
-            "https://www.reddit.com/r/MapPorn/wiki/meta/contest#wiki_submit_a_map"
-
     socmediadict = GenericPost(image_file, title).post_to_all_social()
     my_diag.tweet = socmediadict['tweet_url']
     log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=1)
