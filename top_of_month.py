af5f20bf878f8fd697e4e0feceacb69ca18d2523
petrarch1603 <petrarch1603@gmail.com>
2018-09-07 14:03:53 -0600

    update to use the ShotgunBlast class

--- top_of_month.py
+++ top_of_month.py
@@ -9,11 +9,14 @@ r = praw.Reddit('bot1')
 top_month = list(r.subreddit('mapporn').top('month', limit=1))[0]
 announce_month = 'Top post of the month:\n'
 my_diag = Diagnostic(script=str(os.path.basename(__file__)))
+my_diag.raw_id = top_month.id
 log_db = LoggingDB()
 
 try:
-    s_b = ShotgunBlast(praw_obj=top_month, announce_input=announce_month).post_to_all_social()
-    my_diag.tweet = s_b['tweet_url']
+    s_b = ShotgunBlast(praw_obj=top_month, announce_input=announce_month)
+    assert s_b.check_integrity() == "PASS"
+    s_b_dict = s_b.post_to_all_social()
+    my_diag.tweet = s_b_dict['tweet_url']
     log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=1)
 except Exception as ex:
     my_diag.traceback = "Could not run top_of_month script    \n{}   \n\n".format(ex)
