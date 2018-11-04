f4277ba49ce5429bb6c043592e05e99426d2dc68
petrarch1603 <petrarch1603@gmail.com>
2018-11-04 16:54:42 -0700

    move script into a function

--- WhereWorld.py
+++ WhereWorld.py
@@ -1,7 +1,7 @@
 # This is a project to have a weekly social media trivia post. This python script will
 # run a bot that posts a map image to social media and users will guess the location of
-# the map. The idea is to post it every Wednesday, hence the name "Where in the world
-# Wednesday."
+# the map. The idea is to post it every Friday, hence the name "Where in the world
+# Friday."
 
 import classes
 import csv
@@ -18,49 +18,56 @@ post_message = "#WhereInTheWorld #MapPorn\n" \
 log_db = classes.LoggingDB()
 my_diag = classes.Diagnostic(script=str(os.path.basename(__file__)))
 
-try:
-    # Get the week and year as a format for indexing image
+
+def get_image_name():
     now = datetime.datetime.now()
     thisweek = str(now.isocalendar()[1]).zfill(2)
     two_digit_year = now.strftime('%y')
-    image_number = (str(two_digit_year) + str(thisweek))
-    image_file_name = (image_number + '.png')
-    print(image_file_name)
+    image_num = (str(two_digit_year) + str(thisweek))
+    return image_num
 
-    # Post to Social Media
-    os.chdir('WW')
 
-    socmediadict = classes.GenericPost(image_file_name, post_message).post_to_all_social()
-    my_diag.tweet = socmediadict['tweet_url']
-    os.chdir('..')
-    with open('data/locations.csv') as current_csv:
-        csvreader = csv.reader(current_csv)
-        for row in csvreader:
-            if image_number == row[0]:
-                true_location = row[1]
-                functions.send_reddit_message_to_self(title="Where in world answer",
-                                                      message='The correct location is: ' + str(true_location) + '    '
-                                                              '\nThe Twitter thread is here: ' +
-                                                              str(my_diag.tweet))
-    log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=1)
+def main():
+    try:
+        image_number = get_image_name()
+        image_file_name = image_number + '.png'
+        # Post to Social Media
+        os.chdir('WW')
 
-except functions.tweepy.TweepError as e:
-    if str(e) == 'Unable to access file: No such file or directory':
+        socmediadict = classes.GenericPost(image_file_name, post_message).post_to_all_social()
+        my_diag.tweet = socmediadict['tweet_url']
         os.chdir('..')
-        error_message = "No where in the world maps left. Add more!     \n{}    \n\n".format(e)
-        my_diag.traceback = error_message
-        functions.send_reddit_message_to_self(title="No where world maps left", message=error_message)
+        with open('data/locations.csv') as current_csv:
+            csvreader = csv.reader(current_csv)
+            for row in csvreader:
+                if image_number == row[0]:
+                    true_location = row[1]
+                    functions.send_reddit_message_to_self(
+                        title="Where in world answer",
+                        message='The correct location is: ' + str(true_location) + '    '
+                                '\nThe Twitter thread is here: ' +
+                                str(my_diag.tweet))
+        log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=1)
+
+    except functions.tweepy.TweepError as e:
+        if str(e) == 'Unable to access file: No such file or directory':
+            os.chdir('..')
+            error_message = "No where in the world maps left. Add more!     \n{}    \n\n".format(e)
+            my_diag.traceback = error_message
+            functions.send_reddit_message_to_self(title="No where world maps left", message=error_message)
+            log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=0)
+        else:
+            error_message = e
+            my_diag.traceback = error_message
+            functions.send_reddit_message_to_self(title='ERROR posting Where World', message=error_message)
+            log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=0)
+    except Exception as e:
+        os.chdir('..')
+        my_diag.traceback = "error:    \n{}    \n\n".format(e)
+        my_diag.severity = 2
+        functions.send_reddit_message_to_self(title="Error with WhereWorld", message=my_diag.traceback)
         log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=0)
-    else:
-        error_message = e
-        my_diag.traceback = error_message
-        functions.send_reddit_message_to_self(title='ERROR posting Where World', message=error_message)
-        log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=0)
-        print(e)
-except Exception as e:
-    os.chdir('..')
-    my_diag.traceback = "error:    \n{}    \n\n".format(e)
-    my_diag.severity = 2
-    functions.send_reddit_message_to_self(title="Error with WhereWorld", message=my_diag.traceback)
-    log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=0)
-    print(e)
+
+
+if __name__ == '__main__':
+    main()
