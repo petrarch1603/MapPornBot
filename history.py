ec9d99c4854a65e9e734ddbe1625fe908d6eb08d
petrarch1603 <petrarch1603@gmail.com>
2018-11-04 17:31:02 -0700

    Big changes to script,
    Gets a maprow object instead of strings and numbers. Runs the script thru the object methods instead of lots of procedural code.

--- history.py
+++ history.py
@@ -1,56 +1,32 @@
 import calendar
-from classes import *
+import classes
 import datetime
-from functions import send_reddit_message_to_self
 import os
-import praw
 import random
 
 
-print("Running This Day in History script")
-
-
-def init():
-    global hist_db, log_db, my_diag, r, today_int
-    hist_db = HistoryDB()
-    log_db = LoggingDB()
-    my_diag = Diagnostic(script=os.path.basename(__file__))
-    my_diag.table = 'historymaps'
-    r = praw.Reddit('bot1')
+def get_todays_date():
     today_int = datetime.datetime.now().timetuple().tm_yday
     if today_int > 60 and calendar.isleap(datetime.datetime.now().year):
         today_int -= 1
+    return today_int
 
 
-init()
-today_list = hist_db.get_rows_by_date(today_int)
+def main():
+    hist_db = classes.HistoryDB()
+    today_list = hist_db.get_rows_by_date(get_todays_date())
 
-if len(today_list) == 0:
-    log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=1)
-    log_db.close()
-    exit()
+    if len(today_list) == 0:
+        my_diag = classes.Diagnostic(script=os.path.basename(__file__))
+        my_diag.table = 'historymaps'
+        log_db = classes.LoggingDB()
+        log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=1)
+        log_db.close()
+        exit()
 
-# Check if there are multiple entries with today's date
-random_int = 0
-if len(today_list) > 1:
-    # TODO: change this to random.choice
-    random_int = random.randint(0, (len(today_list) - 1))
+    maprow = random.choice(today_list)
+    maprow.post_to_social_media(table=hist_db.table, script=os.path.basename(__file__))
 
-raw_id = today_list[random_int][0][-6:]
-my_diag.raw_id = raw_id
-praw_obj = r.submission(id=raw_id)
-my_title = today_list[random_int][1]
-my_diag.title = my_title
 
-try:
-    s_b = ShotgunBlast(praw_obj, title=my_title)
-    assert s_b.check_integrity() == "PASS"
-    s_b_dict = s_b.post_to_all_social()
-    my_diag.tweet = s_b_dict['tweet_url']
-    log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=1)
-except Exception as shotgun_error:
-    errorMessage = ('Error encountered: \n' + str(shotgun_error))
-    my_diag.severity = 2
-    my_diag.traceback = errorMessage
-    log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=0, error_text=errorMessage)
-    send_reddit_message_to_self(title="Problem posting this day in history", message=errorMessage)
+if __name__ == '__main__':
+    main()
