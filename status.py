c3dc5ea3daba6df0b8c7fdbe616c603097085be8
petrarch1603 <petrarch1603@gmail.com>
2018-11-07 04:32:30 -0700

    minor formatting

--- status.py
+++ status.py
@@ -146,6 +146,7 @@ def test_functions():
         my_diag.severity = 1
         log_db.add_row_to_db(diagnostics=my_diag.make_dict(), passfail=0)
         my_diag = classes.Diagnostic(script=str(os.path.basename(__file__)))  # Re-initialize the diagnostic
+
     try:    # Test split_message()
         assert functions.split_message("https://redd.it/9cmxi1\ntext goes here\n12") == \
                ['https://redd.it/9cmxi1', 'text goes here', '12']
