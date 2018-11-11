7dd32d4696d09f93f15bcf5eb53e14aac17cc353
petrarch1603 <petrarch1603@gmail.com>
2018-11-11 14:38:59 -0700

    add doc strings

--- GridCollage/GridCollage.py
+++ GridCollage/GridCollage.py
@@ -11,12 +11,27 @@ foreground = Image.open("img/grid.png")
 
 
 def get_file_name(url: str) -> str:
+    """Get the base filename from a URL
+
+    :param url:
+    :type url: str
+    :return: base filename
+    :rtype: str
+
+    """
     path = urlparse(url).path
     return os.path.basename(path)
 
 
 def get_images(url_list: list) -> list:
+    """Get a list of cropped image objects
 
+    :param url_list:List of URL's
+    :type url_list: list of strings
+    :return: List of cropped Image objects
+    :rtype: list
+
+    """
     cropped_images = []
     for i in url_list:
         url = i
@@ -28,7 +43,15 @@ def get_images(url_list: list) -> list:
     return cropped_images
 
 
-def crop_image(image_obj: Image) -> Image:  # Images should be 366 X 366
+def crop_image(image_obj: object) -> object:  # Images should be 366 X 366
+    """Crop images to 366 x 366 pixels
+
+    :param image_obj: Image object
+    :type image_obj: object
+    :return: cropped Image object
+    :rtype: object
+
+    """
     ratio = image_obj.size[0] / image_obj.size[1]
     resize_tup = (int(500*ratio), 500)
     resized_img = image_obj.resize(resize_tup)
@@ -37,8 +60,17 @@ def crop_image(image_obj: Image) -> Image:  # Images should be 366 X 366
     return resized_img.crop(crop_box)
 
 
-def add_text(image_obj: Image, contest_month: str) -> Image:
+def add_text(image_obj: object, contest_month: str) -> object:
+    """Add text on top of image
 
+    :param image_obj: Image object
+    :type image_obj: object
+    :param contest_month: Contest Month
+    :type contest_month: str
+    :return: Image Object with text on top
+    :rtype: Image
+
+    """
     # Each character is approximately 30 pixels
     # Therefore to adjust the text to the center, we add 30 pixels to each character
     word_length = len(contest_month)
@@ -55,6 +87,14 @@ def add_text(image_obj: Image, contest_month: str) -> Image:
 
 
 def main(url_list: list) -> str:
+    """ Main function to turn list of URL strings into a contest advertisement image
+
+    :param url_list: list of URL's (strings)
+    :type url_list: list
+    :return: filepath
+    :rtype:str
+
+    """
     my_cropped_list = get_images(url_list)
     background = Image.new('RGB', (1150, 1150))
     i = 0
