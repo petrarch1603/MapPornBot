1e2463fb9d6401eb4d1e23524855ac6c18c1568a
petrarch1603 <petrarch1603@gmail.com>
2018-12-08 18:54:41 -0700

    re-write the script to use the list of maprows from contest database

--- voting_bestofyear.py
+++ voting_bestofyear.py
@@ -1,114 +1,100 @@
-import csv
+"""Script for creating a voting post for best maps of the year."""
+
+import classes
+import functions
 import os
+import praw
+import random
 import fnmatch
 from datetime import datetime, timedelta
-from functions import *
+
 
 input('Are you ready?')
 
+bot_disclaimer_text = functions.bot_disclaimer()
 contest_year = (datetime.now() - timedelta(days=10)).year
-
-## REMOVE HASHTAGS AFTER 2017 CONTEST GOES LIVE
-# Create CSV with all final contestants from current year
-finalistsCSV = "/SubmissionsArchive/finalists" + str(contest_year) + ".csv"
-finalists = open(finalistsCSV, "a")
-submdir = './SubmissionsArchive'
-for filename in os.listdir(submdir):
-    if fnmatch.fnmatch(filename, '2018*WINNERS.csv'):
-        print(filename)
-        f = open(submdir + "/" + filename)
-        for line in f:
-            finalists.write(line)
-        f.close()
-finalists.close()
-
-# # Prepare the self text of the voting post
-YearVotingText = open('data/year_end_votingtext.txt', 'r').read()
-botDisclaimerText = bot_disclaimer()
-
-# Include the number of submissions and the contest end date in the text of the post.
-# This code makes the end date next Sunday.
-
-next_sunday = next_weekday(datetime.now(), 6)  # Pycharm doesn't like the .now(), but in testing seems it should work.
-next_sunday = next_sunday.strftime('%A %B %d, %Y')
-numbersubmitted = sum(1 for line in open(finalistsCSV))
-YearVotingText = YearVotingText.replace('%NUMBERSUBMITTED%', str(numbersubmitted))
-YearVotingText = YearVotingText.replace('%ENDDATE%', str(next_sunday))
-YearVotingText = YearVotingText.replace('%MYREDDITID%', my_reddit_ID)
+cont_db = classes.ContestDB()
+finalists_list = cont_db.get_top_posts_of_year()
+r = praw.Reddit('bot1')
 
 
-# # Submit the post to Reddit
-post_message = 'Vote Now for the best map of ' + str(contest_year) + '!'
-submission = r.subreddit('mapporn').submit(post_message, selftext=YearVotingText)  # Submits the post to Reddit
-submission.mod.contest_mode()
-submission.mod.distinguish()
-shortlink = submission.shortlink
-send_reddit_message_to_self(title='Reminder', message='[Remember to request Reddit Gold](https://redd.it/7gqgak)    +\n/r/MapPorn /u/Petrarch1603 ' + shortlink)
+def prepare_voting_text():
+    """Prepares the self text for the voting post
 
-# # One by one add a comment to the post, each comment being a map to vote on
-f = open(finalistsCSV, 'r')
-reader = csv.reader(f)
-title_to_finalist = 'The Annual Best Map of the Year contest is now live!'
-message_to_finalist = ('**The Annual Best Map of the Year contest is now live!**    \nThank you for contributing a map. [The voting on the contest is open now at this link.]('
-                               + shortlink + ')    \n' + botDisclaimerText)
-for row in reader:
-    submission.reply('[' + row[0] + '](' + row[1] + ')   \n' + row[2] + '\n\n----\n\n^^^^' + row[4])
-    # the brackets and parentheses are for hyperlinking the name, row[4] is the unique ID of the submission message,
-    # in the congratulations.py program the bot will parse these comments looking for this code and use it to determine
-    # the winners.
+    :return: text of voting post
+    :rtype: str
 
-    # Now send a message to each contestant letting them know it's live.
-    print(str(row[3]))
+    """
+    with open('data/year_end_votingtext.txt', 'r') as my_file:
+        year_voting_text = my_file.read()
+    next_sunday = functions.next_weekday(datetime.now(), 6)
+    next_sunday = next_sunday.strftime('%A %B %d, %Y')
+    numbersubmitted = len(finalists_list)
+    year_voting_text = year_voting_text.replace('%NUMBERSUBMITTED%', str(numbersubmitted))
+    year_voting_text = year_voting_text.replace('%ENDDATE%', str(next_sunday))
+    year_voting_text = year_voting_text.replace('%MYREDDITID%', functions.my_reddit_ID)
+    return year_voting_text
+
+
+def main():
+    """Main script to run voting post"""
+    error_message = ''
+    year_voting_text = prepare_voting_text()
+    post_message = 'Vote Now for the best map of ' + str(contest_year) + '!'
+    submission = r.subreddit('mapporn').submit(post_message, selftext=year_voting_text)  # Submits the post to Reddit
+    submission.mod.contest_mode()
+    submission.mod.distinguish()
+    shortlink = submission.shortlink
+    functions.send_reddit_message_to_self(title='Reminder', message='[Remember to request Reddit Gold]'
+                                                                    '(https://redd.it/7gqgak)    +\n/r/MapPorn '
+                                                                    '/u/Petrarch1603 ' + shortlink)
+    title_to_finalist = 'The Annual Best Map of the Year contest is now live!'
+    message_to_finalist = ('**The Annual Best Map of the Year contest is now live!**    \nThank you for contributing a '
+                           'map. [The voting on the contest is open now at this link.](' + shortlink + ')    \n' +
+                           bot_disclaimer_text)
+    for map_row in finalists_list:
+        submission.reply('[' + str(map_row.map_name) + '](' + str(map_row.url) + ')   \n'
+                         '' + str(map_row.desc) + '\n\n----\n\n^^^^' + str(map_row.raw_id))
+
+        try:
+            r.redditor(map_row.author).message(title_to_finalist, message_to_finalist)
+        except Exception as e:
+            print('Error sending message to ' + map_row.author + '   \n' + str(e))
+    generalcomment = submission.reply('General Comment Thread')
+    generalcomment.mod.distinguish(sticky=True)
+    generalcomment.reply('**What is with the ^^^small characters?**    \nThis contest is automated with a bot. The bot '
+                         'uses these random characters to index the maps and to calculate the winner at the end of the '
+                         'contest.\n\n----\n\n ^^^[Github](https://github.com/petrarch1603/MapPornBot)')
+
+    # Get random image_file_name
+    imagecount = len([name for name in os.listdir('voteimages/')])  # counts how many images are in the directory
+    randraw = random.randint(1, imagecount)  # Creates a random number between 1 and the image count.
+    # Return a random number with a leading zero if necessary. (i.e. 02 instead of 2)
+    image_file_name = str(randraw).zfill(2)
+    # Look in the directory and create a list of files with the name of the image.
+    #
+    # It's not elegant code, but it returns a full file name (i.e. 02.png instead of 02).
+    # The problem is that there are multiple file exensions: jpg, png, jpeg, etc.
+    # There is probably a better way to do it, but for now it works.
+    image_file_name = fnmatch.filter(os.listdir('voteimages/'), image_file_name + '.*')
+    image_file_name = image_file_name[0]
+
+    # Post to social media
+    # Change the message so it includes URL of the Reddit voting post.
+    post_message_url = (post_message + '\n' + shortlink + '\n#MapPorn #Cartography #Contest')
+    image_file_name = ('voteimages/' + image_file_name)
     try:
-        r.redditor(row[3]).message(title_to_finalist, message_to_finalist)
-    except:
-        print('Error sending message to ' + row[3])
-generalcomment = submission.reply('General Comment Thread')  # Have a general comment thread so
-                                                             # people don't post top level comments.
-generalcomment.mod.distinguish(sticky=True)
-generalcomment.reply('**What is with the ^^^small characters?**    \n'
-                     'This contest is automated with a bot. The bot uses these random characters to index the maps and '
-                     'to calculate the winner at the end of the contest.\n\n----\n\n ^^^[Github](https://github.com/petrarch1603/MapPornBot)')
-f.close()
+        social_media_post = classes.GenericPost(filename=image_file_name, title=post_message_url)
+        socialmediadict = social_media_post.post_to_all_social()
+        functions.send_reddit_message_to_self('New Voting Post Posted',
+                                              'A new votingpost.py has been run. Check the post to make'
+                                              ' sure the bot did it right.   \nHere\'s the link to the '
+                                              'post: ' + shortlink + '   \nHere\'s the social media '
+                                              'links:    \n' + str(socialmediadict['tweet_url']))
+    except Exception as e:
+        error_message += "Could not post results to social media.   \n{}    \n\n".format(str(e))
 
-
-# # Need to save the voting post raw_id for use in parsing the winner after a few days.
-raw_id = submission.id_from_url(shortlink)
-file = open('data/votingpostdata.txt', 'w')
-file.write(raw_id)
-file.close()
-
-
-# # Need to post a "Vote Now' advertisement to social media
-# I created a bunch of "Vote Now" posters for use in social media posts.
-# They are all in the 'voteimages' directory and have simple two digit file names: 01.png, 02.png, 03.png etc.
-# Each month a random image will be posted to social media.
-
-# Get random image_file_name
-imagecount = len([name for name in os.listdir('voteimages/')]) # counts how many images are in the directory
-randraw = random.randint(1, imagecount)  # Creates a random number between 1 and the image count.
-# Return a random number with a leading zero if necessary. (i.e. 02 instead of 2)
-image_file_name = str(randraw).zfill(2)
-# Look in the directory and create a list of files with the name of the image.
-#
-# It's not elegant code, but it returns a full file name (i.e. 02.png instead of 02).
-# The problem is that there are multiple file exensions: jpg, png, jpeg, etc.
-# There is probably a better way to do it, but for now it works.
-image_file_name = fnmatch.filter(os.listdir('voteimages/'), image_file_name + '.*')
-image_file_name = image_file_name[0]  # There should only be one image with that name, so this returns the name of that file.
-
-# Post to social media
-# Change the message so it includes URL of the Reddit voting post.
-post_message_url = (post_message + '\n' + shortlink + '\n#MapPorn #Cartography #Contest')
-image_file_name = ('voteimages/' + image_file_name)
-
-# Run a function to post it to different social media accounts
-socialmedialinks = generic_post(image_file_name, post_message_url)
-
-# # Send a Reddit message to me with a summary and links to social media posts
-send_reddit_message_to_self(title_to_finalist, (message_to_finalist + '\nSocMed links: ' + socialmedialinks))
-
-submission.mod.approve()  # Unsure if these two work
-submission.mod.sticky()
-
-
+    if error_message != '':
+        functions.send_reddit_message_to_self(title="error", message=error_message)
+    submission.mod.approve()  # Unsure if these two work
+    submission.mod.sticky()
