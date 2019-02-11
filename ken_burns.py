6c0909ada9d40b6176fb6a6c80385a75b4b80252
patrickmcgranaghan <petrarch1603@gmail.com>
2019-02-10 20:57:09 -0700

    width divisible by 2

--- ken_burns.py
+++ ken_burns.py
@@ -10,14 +10,20 @@ im = Image.open('img/paper.jpg')
 image_width = im.size[0]
 image_height = im.size[1]
 ratio = image_height/image_width
-my_box_dim = [int(300/ratio), 300]
+
+# Width must be divisible by 2 to encode
+my_box_width = int(300/ratio)
+if my_box_width % 2 == 1:
+    my_box_width += 1
+
+my_box_dim = [my_box_width, 300]
 
 initial_width = int(image_width*.05)
 initial_height = int(image_height*.05)
 
 box = [initial_width, initial_height, (my_box_dim[0]+initial_width), (my_box_dim[1]+initial_height)]
 
-full_speed = int(min(image_height, image_width)/200)
+full_speed = int(min(image_height, image_width)/300)
 print(full_speed)
 accel_speed = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05,
                0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
@@ -35,7 +41,7 @@ accel_speed = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05,
 my_frames = []
 i = 1
 for v in accel_speed:
-    name = ('temp/' + ('%0*d' % (3, i)) + '.png')
+    name = ('temp/' + ('%0*d' % (4, i)) + '.png')
     my_speed = int(full_speed * v) + 1
     box[0] = box[0] + my_speed
     box[1] = box[1] + my_speed
@@ -49,8 +55,8 @@ for v in accel_speed:
 decel_speed = accel_speed
 decel_speed.reverse()
 
-for _ in range(100):
-    name = ('temp/' + ('%0*d' % (3, i)) + '.png')
+for _ in range(200):
+    name = ('temp/' + ('%0*d' % (4, i)) + '.png')
     box[0] = box[0] + full_speed
     box[1] = box[1] + full_speed
     box[2] = box[2] + full_speed
@@ -61,7 +67,7 @@ for _ in range(100):
     my_frames.append(frame)
 
 for v in decel_speed:
-    name = ('temp/' + ('%0*d' % (3, i)) + '.png')
+    name = ('temp/' + ('%0*d' % (4, i)) + '.png')
     my_speed = int(full_speed * v) + 1
     box[0] = box[0] + my_speed
     box[1] = box[1] + my_speed
@@ -78,3 +84,4 @@ if os.path.isfile(my_movie_path):
 
 # For some reason this script won't run in Pycharm
 cmd = subprocess.Popen(['bash', 'shell_scripts/make_movie.sh'])
+print(cmd)
