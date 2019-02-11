071d56f6cb1bea26619ee6829437e3607879b71b
patrickmcgranaghan <petrarch1603@gmail.com>
2019-02-10 17:15:54 -0700

    remove create_gif function

--- ken_burns.py
+++ ken_burns.py
@@ -18,7 +18,7 @@ initial_height = int(image_height*.05)
 box = [initial_width, initial_height, (my_box_dim[0]+initial_width), (my_box_dim[1]+initial_height)]
 
 full_speed = int(min(image_height, image_width)/200)
-
+print(full_speed)
 accel_speed = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05,
                0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
                0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
@@ -32,23 +32,15 @@ accel_speed = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05,
                0.9, 0.9]
 
 
-def create_gif(frames_list):
-    frames_list[0].save('temp/temp.gif',
-                        save_all=True,
-                        append_images=frames_list[1:],
-                        loop=1,
-                        minimize_size=True,
-                        allow_mixed=True)
-
-
 my_frames = []
 i = 1
 for v in accel_speed:
     name = ('temp/' + ('%0*d' % (3, i)) + '.png')
-    box[0] = box[0] + int(full_speed * v)
-    box[1] = box[1] + int(full_speed * v)
-    box[2] = box[2] + int(full_speed * v)
-    box[3] = box[3] + int(full_speed * v)
+    my_speed = int(full_speed * v) + 1
+    box[0] = box[0] + my_speed
+    box[1] = box[1] + my_speed
+    box[2] = box[2] + my_speed
+    box[3] = box[3] + my_speed
     frame = im.resize(my_box_dim, box=box)
     frame.save(name, format='png')
     i += 1
@@ -70,16 +62,16 @@ for _ in range(100):
 
 for v in decel_speed:
     name = ('temp/' + ('%0*d' % (3, i)) + '.png')
-    box[0] = box[0] + int(full_speed * v)
-    box[1] = box[1] + int(full_speed * v)
-    box[2] = box[2] + int(full_speed * v)
-    box[3] = box[3] + int(full_speed * v)
+    my_speed = int(full_speed * v) + 1
+    box[0] = box[0] + my_speed
+    box[1] = box[1] + my_speed
+    box[2] = box[2] + my_speed
+    box[3] = box[3] + my_speed
     frame = im.resize(my_box_dim, box=box)
     frame.save(name, format='png')
     i += 1
     my_frames.append(frame)
 
-create_gif(my_frames)
 my_movie_path = 'temp/temp.mp4'
 if os.path.isfile(my_movie_path):
     os.remove(my_movie_path)
